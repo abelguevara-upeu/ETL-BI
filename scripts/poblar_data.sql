94fa1b0210512f7c6240ce9af51226b05a18b73e
abelguevarah <abelguevara@upeu.edu.pe>
2025-09-30 17:26:05 -0500

    fix(poblar_datamart): update script

--- scripts/poblar_data.sql
+++ scripts/poblar_data.sql
@@ -1,10 +1,15 @@
-/* Populate Dim_Transportista table */
+/* Poblar tabla Dim_Transportista */
 
-INSERT INTO Northwind_Mart.dbo.Dim_Transportista (Transportista_Nombre, Transportista_Codigo)
-SELECT CompanyName, ShipperID
+INSERT INTO Northwind_Mart.dbo.Dim_Transportista (
+    Transportista_Nombre,
+    Transportista_Codigo
+)
+SELECT
+    CompanyName,
+    ShipperID
 FROM Northwind.dbo.Shippers;
 
-/* Populate Dim_Cliente table */
+/* Poblar tabla Dim_Cliente */
 INSERT INTO Northwind_Mart.dbo.Dim_Cliente (
     Cliente_Compania, Cliente_Codigo, Cliente_Contacto, Cliente_Titulo_Cont,
     Cliente_Direccion, Cliente_Ciudad, Cliente_Region, Cliente_CodigoPostal,
@@ -24,7 +29,7 @@ SELECT
     Fax
 FROM Northwind.dbo.Customers;
 
-/* Populate Dim_Tiempo table */
+/* Poblar tabla Dim_Tiempo */
 INSERT INTO Northwind_Mart.dbo.Dim_Tiempo (
     Tiempo_DiaSemana, Tiempo_Trimestre, Tiempo_Dia_Annio, Tiempo_Semana_Annio,
     Tiempo_Mes, Tiempo_Annio, Tiempo_Fecha, Tiempo_MesAnnio
@@ -41,9 +46,10 @@ SELECT DISTINCT
 FROM Northwind.dbo.Orders s
 WHERE s.ShippedDate IS NOT NULL;
 
-/* Populate Dim_Producto table */
+/* Poblar tabla Dim_Producto */
 INSERT INTO Northwind_Mart.dbo.Dim_Producto (
-    Producto_Nombre, Producto_Sustituto, Producto_Categoria, Producto_Codigo, Producto_PrecioUnit
+    Producto_Nombre, Producto_Sustituto, Producto_Categoria,
+    Producto_Codigo, Producto_PrecioUnit
 )
 SELECT
     p.ProductName,
@@ -55,7 +61,7 @@ FROM Northwind.dbo.Products p
 INNER JOIN Northwind.dbo.Suppliers s ON p.SupplierID = s.SupplierID
 INNER JOIN Northwind.dbo.Categories c ON p.CategoryID = c.CategoryID;
 
-/* Populate Dim_Empleado table */
+/* Poblar tabla Dim_Empleado */
 INSERT INTO Northwind_Mart.dbo.Dim_Empleado (
     Empleado_Nombre, Empleado_FechaCont, Empleado_Codigo
 )
@@ -65,7 +71,7 @@ SELECT
     EmployeeID
 FROM Northwind.dbo.Employees;
 
-/* Populate Fact_Ventas table */
+/* Poblar tabla Fact_Ventas */
 INSERT INTO Northwind_Mart.dbo.Fact_Ventas (
     Tiempo_key, Cliente_key, Transportista_key, Producto_key, Empleado_key,
     FechaRequerida, Flete_lineaItem, Total_lineaItem, Cantidad_lineaItem, Descuento_lineaItem
@@ -77,15 +83,25 @@ SELECT
     p.Producto_key,
     e.Empleado_key,
     o.RequiredDate,
-    o.Freight * od.Quantity / (SELECT SUM(Quantity) FROM Northwind.dbo.[Order Details] od2 WHERE od2.OrderID = o.OrderID) AS Flete_linialtem,
-    od.UnitPrice * od.Quantity AS Total_liniealtemp,
+    (o.Freight * od.Quantity /
+        (
+        SELECT SUM(Quantity) FROM Northwind.dbo.[Order Details] od2 WHERE od2.OrderID = o.OrderID
+        )
+    ) AS Flete_lineaItem, /* Calculo de flete por línea de item */
+    od.UnitPrice * od.Quantity AS Total_lineaItem, /* Total sin descuento */
     od.Quantity AS Cantidad_lineaItem,
-    od.Discount * od.UnitPrice * od.Quantity AS Descuento_lineaItem
+    od.Discount * od.UnitPrice * od.Quantity AS Descuento_lineaItem /* Descuento aplicado */
 FROM Northwind.dbo.Orders o
-INNER JOIN Northwind.dbo.[Order Details] od ON o.OrderID = od.OrderID
-INNER JOIN Northwind_Mart.dbo.Dim_Producto p ON od.ProductID = p.Producto_Codigo
-INNER JOIN Northwind_Mart.dbo.Dim_Cliente c ON o.CustomerID COLLATE Modern_Spanish_CI_AS = c.Cliente_Codigo COLLATE Modern_Spanish_CI_AS
-INNER JOIN Northwind_Mart.dbo.Dim_Tiempo t ON o.ShippedDate = t.Tiempo_Fecha
-INNER JOIN Northwind_Mart.dbo.Dim_Transportista tr ON o.ShipVia = tr.Transportista_Codigo
-INNER JOIN Northwind_Mart.dbo.Dim_Empleado e ON o.EmployeeID = e.Empleado_Codigo
+INNER JOIN Northwind.dbo.[Order Details] od
+    ON o.OrderID = od.OrderID
+INNER JOIN Northwind_Mart.dbo.Dim_Producto p
+    ON od.ProductID = p.Producto_Codigo
+INNER JOIN Northwind_Mart.dbo.Dim_Cliente c
+    ON o.CustomerID COLLATE Modern_Spanish_CI_AS = c.Cliente_Codigo COLLATE Modern_Spanish_CI_AS
+INNER JOIN Northwind_Mart.dbo.Dim_Tiempo t
+    ON o.ShippedDate = t.Tiempo_Fecha
+INNER JOIN Northwind_Mart.dbo.Dim_Transportista tr
+    ON o.ShipVia = tr.Transportista_Codigo
+INNER JOIN Northwind_Mart.dbo.Dim_Empleado e
+    ON o.EmployeeID = e.Empleado_Codigo
 WHERE o.ShippedDate IS NOT NULL;
